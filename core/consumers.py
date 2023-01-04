67048159864f5a6e6d40c78a783eec56c5acefbd
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-01-04 08:54:39 +0545

    feat: add sync and async consumer for responding to client message

--- core/consumers.py
+++ core/consumers.py
@@ -11,6 +11,7 @@ class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
         """It is a handler that is called when a client initially opens a connection and is about to end the websocket handshake"""
         print("Websocket connected: ....", event)
+        # websocket.accept is used to accept the connection
         self.send({
             'type':"websocket.accept",
         })
@@ -42,3 +43,51 @@ class MyAsyncConsumer(AsyncConsumer):
         """It is a async handler for disconnecting the websocket connection"""
         print("Websocket disconneced", event)
         raise StopConsumer()
+
+
+class MySyncConsumer2(SyncConsumer):
+    """Another sync websocket"""
+    def websocket_connect(self, event):
+        print("++++Sync Websocket connected++++")
+        print("++++", event, "++++")
+        # accepting the connection opened initally by the client
+        self.send({
+            'type':'websocket.accept',
+        })
+
+    def websocket_receive(self, event):
+        """Receiving the message from client and also responding to the client"""
+        print("++++Message Received++++")
+        print("++++Received Message is: ", event['text'], "++++")
+        self.send({
+            "type": "websocket.send",
+            "text": event["text"],
+        })
+
+    def websocket_disconnect(self, event):
+        """Disconnecting the websocket connection"""
+        print(f"++++Web socket disconnected++++:+++{event}")
+        raise StopConsumer()
+
+
+class MyAsyncConsumer2(AsyncConsumer):
+    """Another example for async consumer with resoponding  the message to the client"""
+    async def websocket_connect(self, event):
+        print("++++Async Websocket connected++++")
+        # accepting the connection
+        await self.send({
+            'type':'websocket.accept',
+        })
+    
+    async def websocket_receive(self, event):
+        print("++++Message Receive++++")
+        print(f"++++{event['text']}++++")
+        await self.send({
+            'type':'websocket.send',
+            'text':'Message was received and this is sent from server'
+        })
+    
+    async def websocket_disconnect(self, event):
+        print(f"++++Web socket disconnected++++:+++{event}")
+        raise StopConsumer()
+    
--- core/routing.py
+++ core/routing.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from core.consumers import MyAsyncConsumer, MySyncConsumer
+from core import consumers
 
 
 app_name = "core"
 websocket_urlpatterns = [
-    path("ws/sc/", MySyncConsumer.as_asgi(), name="mySyncConsumer"),
-    path("ws/ac/", MyAsyncConsumer.as_asgi(), name="myAsyncConsumer"),
+    path("ws/sc/", consumers.MySyncConsumer.as_asgi(), name="mySyncConsumer"),
+    path("ws/ac/", consumers.MyAsyncConsumer.as_asgi(), name="myAsyncConsumer"),
+    path('ws/sc2/', consumers.MySyncConsumer2.as_asgi(), name="mySyncConsumer2"),
+    path('ws/ac2/', consumers.MyAsyncConsumer2.as_asgi(), name="myAsyncConsumer2"),
 ]
