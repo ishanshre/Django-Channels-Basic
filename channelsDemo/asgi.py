e771c757514285450a3a36c5c4caac401097452d
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-01-07 09:06:33 +0545

    feat: add auth middleware stack and display username of sending user

--- channelsDemo/asgi.py
+++ channelsDemo/asgi.py
@@ -12,15 +12,25 @@ import os
 from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 from core import routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channelsDemo.settings')
 
+# application = ProtocolTypeRouter({
+#     "http":get_asgi_application(),
+#     "websocket": URLRouter(
+#         routing.websocket_urlpatterns,
+#     ),
+# })
+
 application = ProtocolTypeRouter({
-    "http":get_asgi_application(),
-    "websocket": URLRouter(
-        routing.websocket_urlpatterns,
-    ),
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns,
+        )
+    )
 })
--- chatApp2/consumers.py
+++ chatApp2/consumers.py
@@ -13,6 +13,8 @@ from channels.db import database_sync_to_async
 class MyDynamicGroupAsyncConsumer(AsyncConsumer):
     """Async consumer chat app wit dynamic group"""
 
+
+    # convert django orm from sync to async consumer
     @database_sync_to_async
     def create_chat(self, msg, name):
         group = Group.objects.get(name=name)
@@ -23,6 +25,7 @@ class MyDynamicGroupAsyncConsumer(AsyncConsumer):
         print("channel layer :::", self.channel_layer)
         print("channel name :::", self.channel_name)
         print("Group Name:   ",self.scope['url_route']['kwargs']['groupName'])
+        print("Current User ::::", self.scope['user'])
         self.groupName = self.scope['url_route']['kwargs']['groupName']
         await self.channel_layer.group_add(self.groupName, self.channel_name)
         await self.send({
@@ -35,12 +38,20 @@ class MyDynamicGroupAsyncConsumer(AsyncConsumer):
         data = json.loads(event['text']) # event['text'] is str class so converting into dict
         actual_message = data['msg']
         print("actual message...", actual_message)
+        if self.scope['user'].is_authenticated:
         # django orm is sync. So we must make it aync
-        chat = await self.create_chat(actual_message, self.groupName)
-        await self.channel_layer.group_send(self.groupName, {
-            "type":"chat.message",
-            "message":event['text']
-        })
+            chat = await self.create_chat(actual_message, self.groupName)
+            data['user'] = self.scope['user'].username
+            print(data)
+            await self.channel_layer.group_send(self.groupName, {
+                "type":"chat.message",
+                "message":json.dumps(data)
+            })
+        else:
+            await self.send({
+                "type":"websocket.send",
+                "text": json.dumps({"msg":"Login Required"})
+            })
     
     async def chat_message(self, event):
         print("Message from client::: ", event['message'])
